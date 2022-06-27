9f03eb55e8cdad55241487558c7be0feea7e187d
alehandromaniuk <57686519+OleksandRomaniuk@users.noreply.github.com>
2022-06-27 11:10:51 +0300

    Add FunctionNameTransducer

--- calculator/src/main/java/src/impl/fsm/function/FunctionNameTransducer.java
+++ calculator/src/main/java/src/impl/fsm/function/FunctionNameTransducer.java
@@ -1,39 +1,45 @@
 package src.impl.fsm.function;
 
+import fsm.CharSequenceReader;
+import fsm.ResolvingException;
+import fsm.Transducer;
 
-import src.fsm.Input;
-import src.fsm.Transducer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import src.impl.fsm.function.name.IdentifierMachine;
-import src.impl.fsm.util.FunctionHolder;
-import src.impl.fsm.util.ResolvingException;
+
+import java.util.function.BiConsumer;
 
 
-/**
- *
- * Implementation of {@link Transducer} for {@link StringBuilder} output.
- *
- */
 
-class FunctionNameTransducer implements Transducer<FunctionHolder> {
+public class FunctionNameTransducer<O> implements Transducer<O> {
+
+    private final BiConsumer<O, String> resultConsumer;
+
     private static final Logger logger = LoggerFactory.getLogger(FunctionNameTransducer.class);
 
+    public FunctionNameTransducer(BiConsumer<O, String> resultConsumer) {
+
+        this.resultConsumer = resultConsumer;
+    }
+
     @Override
-    public boolean doTransition(Input inputChain, FunctionHolder outputChain) throws ResolvingException {
+    public boolean doTransition(CharSequenceReader inputChain, O outputChain) throws ResolvingException {
 
         StringBuilder stringBuilder = new StringBuilder();
 
         IdentifierMachine identifierMachine = IdentifierMachine.create();
 
         if (identifierMachine.run(inputChain, stringBuilder)){
+
             if (logger.isInfoEnabled()){
 
                 logger.info("Function name - {}", stringBuilder);
             }
-            outputChain.setFunctionName(stringBuilder.toString());
+
+            resultConsumer.accept(outputChain, stringBuilder.toString());
             return true;
         }
+
         return false;
     }
 
